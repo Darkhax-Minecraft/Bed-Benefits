2a99b2a545c090d444e68fd7d0af4f43c9909eb1
Tyler Hancock <darklime@live.ca>
2024-01-03 07:36:28 -0700

    Port to MC 1.20.2 and add NeoForge support.

---
+++ neoforge/src/main/java/net/darkhax/bedbenefits/BedBenefitsNeoForge.java
@@ -0,0 +1,12 @@
+package net.darkhax.bedbenefits;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class BedBenefitsNeoForge {
+
+    public BedBenefitsNeoForge() {
+
+        new BedBenefitsCommon();
+    }
+}
